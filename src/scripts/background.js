ac8e79d18e78093dbdc859ac32251ed1e844d036
longjxchina <longjxchina@gmail.com>
2013-03-25 14:19:26 +0800

    add base project

---
+++ src/scripts/background.js
@@ -0,0 +1,19 @@
+﻿// 右键菜单
+chrome.contextMenus.create({ title: "乐趣分享" });
+
+var account;
+
+account = {
+    name: "abcd"
+}
+
+// 
+function savePage() {
+    if (!navigator.onLine) {
+        alert("您必须联网才能保存页面！");
+        return;
+    }
+    chrome.tabs.executeScript(null, { code: "showSavedData()" });
+}
+
+chrome.browserAction.onClicked.addListener(savePage);
