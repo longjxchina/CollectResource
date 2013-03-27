1a0ad6a9d0d867cdf772d833eff8c94159f52f75
longjxchina <longjxchina@gmail.com>
2013-03-27 17:15:37 +0800

    complete login

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,10 +1,17 @@
-﻿// 右键菜单
-chrome.contextMenus.create({ title: "乐趣分享" });
-
-var account = {
+﻿var account = {
     name: null,
     token:null
 };
+
+// 右键菜单
+chrome.contextMenus.create({
+    title: "乐趣分享",
+    onclick: function () {
+        if (account.token == null) {
+            window.open("html/login.html", "登录", "width=427,height=367,top=200,left=300");
+        }
+    }
+});
 
 // 
 function savePage() {
